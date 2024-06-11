b995d5dfe56784fc263657bf33bb434638d158d7
maliha20 <gulista.marium20@gmail.com>
2024-06-11 21:10:56 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+
+
+
+
+const app: Application = express()
+
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req: Request, res: Response) => {
+ const a= 10;
+ res.send(a)
+})
+
+app.post('/', (req: Request, res: Response) =>{
+
+})
+
+export default app
---
+++ src/app/config/index.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+import Path from 'path';
+
+dotenv.config({ path: Path.join((process.cwd(), '.env')) });
+
+export default {
+  NODE_ENV : process.env.NODE_ENV,
+  database_url: process.env.DATABASE_URL,
+  port: process.env.PORT,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  default_pass: process.env.DEFAULT_PASS,
+};
